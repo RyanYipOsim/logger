de7591c140a0ff4e88d182fe957a01f5f8be56ad
RyanYipOsim <ryan.yip@osim.com.sg>
2022-02-22 16:39:58 +0800

    🐛: fixes iOS logs not working

--- lib/src/outputs/console_output.dart
+++ lib/src/outputs/console_output.dart
@@ -1,5 +1,13 @@
-import 'package:logger/src/logger.dart';
+//
+
+import 'dart:developer' as developer;
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:logger/src/log_output.dart';
+import 'package:logger/src/logger.dart';
+
+const bool _kReleaseMode = kReleaseMode;
 
 /// Default implementation of [LogOutput].
 ///
@@ -7,6 +15,23 @@ import 'package:logger/src/log_output.dart';
 class ConsoleOutput extends LogOutput {
   @override
   void output(OutputEvent event) {
-    event.lines.forEach(print);
+    if (_kReleaseMode || !Platform.isIOS) {
+      event.lines.forEach(debugPrint);
+    } else {
+      event.lines.forEach(developer.log);
+    }
   }
 }
+
+// import 'package:logger/src/logger.dart';
+// import 'package:logger/src/log_output.dart';
+
+// /// Default implementation of [LogOutput].
+// ///
+// /// It sends everything to the system console.
+// class ConsoleOutput extends LogOutput {
+//   @override
+//   void output(OutputEvent event) {
+//     event.lines.forEach(print);
+//   }
+// }
